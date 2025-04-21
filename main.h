a272294fa1b967752a43bf2a14ca489a62b04716
GuarickGit <10615@holbertonstudents.com>
2025-04-21 21:48:45 +0200

    Add main.h file

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* === Librairies autorisées === */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* === Fonctions utilitaires === */
+char **_realloc_args(char **args, int old_size, int new_size);
+char *get_path_from_env(char **envp);
+
+/* === Fonctions principales === */
+void remove_trailing_newline(char *line);
+char **parse_line(char *line);
+char *find_full_path(char *command, char **envp);
+void execute_command(char *command, char **args, char **envp);
+int handle_builtin(char **args, char **envp, char *line);
+
+#endif /* MAIN_H */
