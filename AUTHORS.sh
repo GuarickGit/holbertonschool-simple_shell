1b41199cf2ea8472cc66a42286cee5c900b07437
Samamtro <fofanasanoussy545@gmail.com>
2025-04-23 02:05:55 +0200

    file to AUTHORS

---
+++ AUTHORS.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Proprietaire: sanoussy fofana
+#Date: 2025-04-25
+#Description: ce script va affricher le messege "WELCOME EVERIYONE" et le nom de l'utilisateur actuel
+
+# Afficher le message "WELCOME EVERYONE"
+echo "Bonjour axel, WELCOME EVERYONE dans le monde du programme shell script"
